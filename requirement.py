ff84a8751971cec6825b93c044a8d73e58f95fa4
TWcamel <j0930495159@gmail.com>
2022-01-03 21:11:10 +0800

    W2 testcase update

--- requirement.py
+++ requirement.py
@@ -13,8 +13,7 @@ calculate(4, 8)  # 你的程式要能夠計算 4+5+6+7+8，最後印出 30
 
 
 def avg(data):
-    print(sum([employee["salary"] for employee in [
-          employees for employees in data["employees"]]])/data["count"])
+    print(sum(employee["salary"] for employee in data["employees"])/data["count"])
 
 
 # 請用你的程式補完這個函式的區塊
@@ -56,6 +55,7 @@ maxProduct([5, 20, 2, 6])  # 得到 120
 maxProduct([10, -20, 0, 3])  # 得到 30
 maxProduct([-1, 2])  # 得到 -2
 maxProduct([-1, 0, 2])  # 得到 0
+maxProduct([-1,-2, 0]) # 得到 2
 
 # %%
 # 4
