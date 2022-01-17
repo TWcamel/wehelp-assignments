910623bf0175b84d707eee3b8814300ae2a375c3
TWcamel <j0930495159@gmail.com>
2022-01-18 00:04:39 +0800

    assignment2 complete

--- W4/app.py
+++ W4/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, request, json, jsonify, url_for, redirect
+import lib.json_lib as jb
+import lib.member_lib as mb
+
 
 app = Flask(__name__, static_url_path='')
 # app = Flask(__name__, static_url_path='', static_folder='../frontend/static')
@@ -6,7 +9,32 @@ app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def run():
-    return "{\"message\":\"Hello World 222\"}"
+    return "{\"message\":\"You are now at lading page\"}"
+
+
+@app.route("/signin", methods=["POST"])
+def signIn():
+    data = request.json
+    member_check_code = mb.check_member(user=data)["status_code"]
+    if member_check_code == 1:
+        return redirect(url_for('member_signin'))
+    elif member_check_code == 0:
+        return redirect(url_for('error_signin', error_message="wrong_account_or_pwd"))
+    elif member_check_code == -1:
+        return redirect(url_for('error_signin', error_message="empty_input"))
+
+
+@app.route("/member")
+def member_signin():
+    return jsonify({"message": "恭喜您，成功登入系統 🙂"})
+
+
+@app.route("/error/<error_message>")
+def error_signin(error_message):
+    if error_message == "wrong_account_or_pwd":
+        return jsonify({"message": "帳號或密碼錯誤，請重新輸入 😢"})
+    elif error_message == "empty_input":
+        return jsonify({"message": "請輸入帳號密碼 😥"})
 
 
 if __name__ == "__main__":
