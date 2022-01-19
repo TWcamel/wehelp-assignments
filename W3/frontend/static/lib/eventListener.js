dc88e2be1ea45a4b5ce95fb4a5e4afa9cf70ae05
TWcamel <j0930495159@gmail.com>
2022-01-19 21:03:55 +0800

    W4

--- W3/frontend/static/lib/eventListener.js
+++
@@ -1,9 +0,0 @@
-let customedAddEventListener = ({ element, data }) => {
-	element.addEventListener("click", () => {
-		createGalleryItemContainer(data, {
-			from: 8,
-			to: 16,
-		});
-		element.remove()
-	});
-};
--- W3/frontend/static/lib/request.js
+++
@@ -1,7 +0,0 @@
-let requestURL = async (url) => {
-	try {
-		return await fetch(url).then((response) => response.json()).then((data) => data.result);
-	} catch (error) {
-		console.error("Error:", error);
-	}
-};
