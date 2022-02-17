0f1c7b23fdb193b641b681bc6311a251c297b011
TWcamel <j0930495159@gmail.com>
2022-02-17 19:38:52 +0800

    W7 finished

--- W4/server/lib/json_lib.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify, json
-
-def print_and_jsonify(data):
-    def decorator(func):
-        def wrapper():
-            print("data is " + format(data))
-            func()
-        return wrapper
-    return decorator
